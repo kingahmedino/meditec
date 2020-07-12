daf2703010b6d38a1645db5dd8d7a2e5d029487e
kingahmedino <mohammedoluwasesan@gmail.com>
2020-07-12 13:26:43 +0100

    First commit - added StartUp screen, Maps Screen and setup Map dependencies

---
+++ app/src/main/java/com/app/meditec/MapsActivity.java
@@ -0,0 +1,15 @@
+package com.app.meditec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapsActivity extends AppCompatActivity {
+    private static final String TAG = "MapsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
